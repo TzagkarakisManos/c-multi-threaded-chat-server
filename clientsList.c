50168877a9ffc8f96d50a91165529dc78a045e5a
TzagkarakisManos <manostzagk2003@gmail.com>
2026-03-22 16:18:09 +0200

    Vibecoded Solution

--- clientsList.c
+++ clientsList.c
@@ -1,65 +1,60 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
 
 #include "clientsList.h"
 #include "msg.h"
+#include "config.h"
 
-void initClientsList(struct clientsList *cl)
-{
-	cl->clients_num = 0;
-	pthread_mutex_init(&cl->clients_lock, NULL);
+void initClientsList(struct clientsList *cl) {
+    cl->clients_num = 0;
+    pthread_mutex_init(&cl->clients_lock, NULL);
 }
 
-int addClient(struct clientsList *cl, int fd)
-{
-	pthread_mutex_lock(&cl->clients_lock);
-	if (cl->clients_num == MAX_CONNECTED_CLIENTS) {
-			pthread_mutex_unlock(&cl->clients_lock);
-			return -1;
-	}
-	cl->clients_fd[cl->clients_num++] = fd;
-	pthread_mutex_unlock(&cl->clients_lock);
-	return 0;
+int addClient(struct clientsList *cl, int fd) {
+    pthread_mutex_lock(&cl->clients_lock);
+    if (cl->clients_num == MAX_CONNECTED_CLIENTS) {
+        pthread_mutex_unlock(&cl->clients_lock);
+        return -1;
+    }
+    cl->clients_fd[cl->clients_num++] = fd;
+    pthread_mutex_unlock(&cl->clients_lock);
+    return 0;
 }
 
-int removeClient(struct clientsList *cl, int fd)
-{
-	int removed = 0;
-	int i = 0;
-	int j;
-
-	pthread_mutex_lock(&cl->clients_lock);
-	while (!removed && i < cl->clients_num) {
-		if (cl->clients_fd[i] == fd) {
-			for (j = i + 1; j < cl->clients_num; ++j) {
-				cl->clients_fd[j - 1] = cl->clients_fd[j];
-			}
-			removed = 1;
-			cl->clients_num--;
-		}
-		++i;
-	}
-	pthread_mutex_unlock(&cl->clients_lock);
-
-	if (!removed)
-		return -1;
-
-	return 0;
+int removeClient(struct clientsList *cl, int fd) {
+    int removed = 0;
+    pthread_mutex_lock(&cl->clients_lock);
+    for (int i = 0; i < cl->clients_num; i++) {
+        if (cl->clients_fd[i] == fd) {
+            for (int j = i + 1; j < cl->clients_num; j++) {
+                cl->clients_fd[j - 1] = cl->clients_fd[j];
+            }
+            cl->clients_num--;
+            removed = 1;
+            break;
+        }
+    }
+    pthread_mutex_unlock(&cl->clients_lock);
+    return removed ? 0 : -1;
 }
 
 int broadcastClientsList(struct clientsList *cl, const char *msg, const char *sender, int sender_fd) {
+    char full_msg[MAX_MSG_LEN + 128];
+    
+    pthread_mutex_lock(&cl->clients_lock);
+    snprintf(full_msg, sizeof(full_msg), "\033[35m%s\033[0m> %s", sender, msg);
 
-	(void) cl;
-	(void) msg;
-	(void) sender;
-	(void) sender_fd;
-
-	//TODO use sendNewMessage to broadcast the message to all clients except the sender of the message
-
-	return 0;
+    for (int i = 0; i < cl->clients_num; i++) {
+        if (cl->clients_fd[i] != sender_fd) {
+            sendMessage(cl->clients_fd[i], full_msg);
+        }
+    }
+    pthread_mutex_unlock(&cl->clients_lock);
+    return 0;
 }
 
-void destroyClientsList(struct clientsList *cl)
-{
-	cl->clients_num = 0;
-	pthread_mutex_destroy(&cl->clients_lock);
+void destroyClientsList(struct clientsList *cl) {
+    pthread_mutex_destroy(&cl->clients_lock);
 }
--- server.c
+++ server.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/un.h>
 #include <arpa/inet.h>
+#include <pthread.h>
 #include <string.h>
-#include <errno.h>
 
 #include "config.h"
 #include "readwrite.h"
@@ -13,6 +12,56 @@
 #include "printMsg.h"
 
 struct clientsList cl;
+
+void *receive_thread(void *arg){
+	if (arg == NULL) {
+        perror("Socket initialization problem");
+		return NULL;
+    }
+	int socket_fd = *(int *)arg;
+	char msg[MAX_MSG_LEN + 1];
+	char print_msg[MAX_PRINT_MSG_LEN + 1];
+
+	while(1){
+		ssize_t read_bytes = recvMessage(socket_fd, msg, MAX_MSG_LEN);
+		if (read_bytes > 0){
+				snprintf(print_msg, sizeof(print_msg), "\033[35mUser\033[0m> %s\n> ", msg);
+				printMsg(stdout, print_msg);
+			} else if (read_bytes == 0) {
+				printf("User disconnected.\n");
+                break;
+			} else {
+				perror("recvMessage failure");
+				break;
+			}
+			strcpy(msg, "");
+	}
+
+	shutdown(socket_fd, SHUT_RDWR);
+	return NULL;	
+}
+
+void *send_thread(void *arg) {
+    int fd = *(int *)arg;
+    char msg[MAX_MSG_LEN + 1];
+
+    while(1) {
+        printf("> ");
+        fflush(stdout);
+
+        if(fgets(msg, MAX_MSG_LEN, stdin) != NULL) {
+            msg[strcspn(msg, "\n")] = 0;
+            if (sendMessage(fd, msg) == -1) {
+                perror("sendMessage failure");
+                break;
+            }
+        } else {	
+            break; 
+        }
+    }
+    shutdown(fd, SHUT_RDWR);
+    return NULL;
+}
 
 int main() {
 	struct sockaddr_in addr;
@@ -25,95 +74,46 @@ int main() {
 	socklen_t client_addr_len = sizeof(client_addr);
 	char client_ip[INET_ADDRSTRLEN];
 
-	char msg[MAX_MSG_LEN + 1];
 	char print_msg[MAX_PRINT_MSG_LEN + 1];
 
-	socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-	if (socket_fd == -1) {
-		perror("socket failure");
-		exit(EXIT_FAILURE);
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) { perror("socket"); exit(1); }
+
+    socket_option = 1;
+    setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &socket_option, sizeof(socket_option));
+
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(socket_fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
+        perror("bind"); exit(1);
+    }
+
+    listen(socket_fd, SOMAXCONN);
+    initClientsList(&cl);
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        client_fd = accept(socket_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_fd == -1) continue; 
+
+        inet_ntop(AF_INET, &client_addr.sin_addr, ip_buf, sizeof(ip_buf));
+        addClient(&cl, client_fd);
+        broadcastClientsList(&cl, "joined the room", ip_buf, -1);
+
+	int *thread_args = malloc(sizeof(int));
+	*thread_args = client_fd;
+
+	pthread_t receive_thread_id;
+	if (pthread_create(&receive_thread_id, NULL, receive_thread, thread_args) != 0){
+		perror("Failed to initialize threads");
 	}
 
-	// necessary option for quick reusage of the port
-	socket_option = 1;
-	if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &socket_option, sizeof(socket_option)) == -1) {
-		close(socket_fd);
-		perror("setsockopt failure");
-		exit(EXIT_FAILURE);
-	}
-
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(PORT);
-	addr.sin_addr.s_addr = INADDR_ANY;
-
-	if (bind(socket_fd, (struct sockaddr *) &addr, sizeof(addr)) == -1) {
-		close(socket_fd);
-		perror("bind failure");
-		exit(EXIT_FAILURE);
-	}
-
-	if (listen(socket_fd, SOMAXCONN) == -1) {
-		close(socket_fd);
-		perror("listen failure");
-		exit(EXIT_FAILURE);
-	}
-
-	printf("Listening on port %d\n", PORT);
-
-	client_fd = accept(socket_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	if (client_fd == -1) {
-		close(socket_fd);
-		perror("accept failure");
-		exit(EXIT_FAILURE);
-	}
-
-	inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, sizeof(client_ip));
-	snprintf(print_msg, sizeof(print_msg), "-- User %s has joined the conversation\n", client_ip);
-	printMsg(stdout, print_msg);
-
-	while (1) {
-		ssize_t read_bytes = recvMessage(client_fd, msg, MAX_MSG_LEN);
-
-		if (read_bytes == 0) {
-			snprintf(print_msg, sizeof(print_msg), "-- User %s has left the conversation\n", client_ip);
-			printMsg(stdout, print_msg);
-			close(client_fd);
-			break;
-		}
-		else if (read_bytes == -1) {
-			perror("recvMessage failure");
-			close(client_fd);
-			close(socket_fd);
-			exit(EXIT_FAILURE);
-		} else if (read_bytes == -2) {
-			snprintf(print_msg, sizeof(print_msg), "error: the message of the client cannot fit in the 'msg' buffer\n");
-			printMsg(stderr, print_msg);
-			close(client_fd);
-			close(socket_fd);
-			exit(EXIT_FAILURE);
-		}
-
-		snprintf(print_msg, sizeof(print_msg), "\033[35mUser %s\033[0m> %s\n", client_ip, msg);
-		printMsg(stdout, print_msg);
-
-		//code for version one (ping-pong)
-		
-		printf("> ");
-		fflush(stdout);
-
-		if(fgets(msg, MAX_MSG_LEN, stdin) != NULL){
-			msg[strcspn(msg, "\n")] = 0;
-			if (sendMessage(client_fd, msg) == -1){
-				perror("sendMessage failure");
-				break;
-			}
-		} else {
-				break;
-			}
-
-		strcpy(msg, "");
-	}
-
+	send_thread(thread_args);
+	shutdown(client_fd, SHUT_RDWR);
+    pthread_join(receive_thread_id, NULL);
+    close(client_fd);
 	close(socket_fd);
 
 	return 0;
